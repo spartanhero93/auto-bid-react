464ec03247447e6d5070399f8b2ea34ec0e8a624
spartanhero93 <spartanhero93@gmail.com>
2021-09-28 22:22:06 -0400

    quick test for sidebar

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react'
 import renderer from 'react-test-renderer'
-import App from './App';
-import { Link } from './Tests/hover';
+import App from './App'
+import Sidebar from './Containers/Sidebar'
+import { Link } from './Tests/hover'
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-// test("Link changes the class when hovered", () => {
-//   const component = renderer.create(
-//     <Link page='https://Google.com'>Google</Link>
-//   )
-//   let tree = component.toJSON()
-// })
+test('Check the sidebar links', () => {
+  const component = renderer.create(<Sidebar />)
+  let tree = component.toJSON()
+  const liArr = tree.children.filter(i => i.type === 'ul')[0].children
+  const [Account, Explore, EndingSoon, MyBids, Support] = liArr
+  expect(Account.children.join('')).toEqual('Account')
+  expect(Explore.children.join('')).toEqual('Explore')
+  expect(EndingSoon.children.join('')).toEqual('Ending Soon')
+  expect(MyBids.children.join('')).toEqual('My Bids')
+  expect(Support.children.join('')).toEqual('Support')
+})
 
 test('one plus two is three', () => {
   expect(1 + 2).toEqual(3)
